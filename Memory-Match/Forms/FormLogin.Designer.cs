72f966e4339e799af5d634d4b96a78da77878d2b
Sandru Gabriel <gabi200611@yahoo.com>
2023-03-23 18:44:07 +0200

    done

---
+++ Memory-Match/Forms/FormLogin.Designer.cs
@@ -0,0 +1,48 @@
+﻿namespace Memory_Match.Forms
+{
+    partial class FormLogin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // FormLogin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 19F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(353, 380);
+            this.Font = new System.Drawing.Font("Microsoft YaHei UI Light", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "FormLogin";
+            this.Text = "FormLogincs";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
---
+++ Memory-Match/Forms/FormLogin.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Memory_Match.Forms
+{
+    public partial class FormLogin : Form
+    {
+
+        Button btnLogin;
+        Button btnSignUp;
+
+        public FormLogin()
+        {
+            InitializeComponent();
+
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+
+            //Button Login
+            btnLogin = new Button();
+            this.Controls.Add(btnLogin);
+
+            Font font = new Font("Microsoft YaHei UI Light", 22);
+
+            this.btnLogin.Location = new System.Drawing.Point(110, 175);
+            this.btnLogin.Size = new System.Drawing.Size(135,60);
+            this.btnLogin.Font = font;
+            this.btnLogin.Text = "Login";
+            this.btnLogin.Click += new EventHandler(btnLogin_Click);
+
+            //Button SignUp
+            btnSignUp = new Button();
+            this.Controls.Add(btnSignUp);
+
+            this.btnSignUp.Location = new System.Drawing.Point(110,275);
+            this.btnSignUp.Size = new System.Drawing.Size(135,60);
+            this.btnSignUp.Font = font;
+            this.btnSignUp.Text = "Sign Up";
+            this.btnSignUp.Click += new EventHandler(btnSignUp_Click);
+
+
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+
+
+
+        }
+
+        private void btnSignUp_Click(object sender, EventArgs e)
+        {
+
+
+
+        }
+
+    }
+}
